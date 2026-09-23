33ae418c2d79e0f40a5d9eb6ae54a88070d980d2
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2026-09-23 04:40:31 +0200

    initial files

---
+++ src/main/java/cute/ame/celsius/Config.java
@@ -0,0 +1,20 @@
+package cute.ame.celsius;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@EventBusSubscriber(modid = Celsius.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+}
